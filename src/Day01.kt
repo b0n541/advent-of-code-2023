2aacad96c091e7124be01e91d41f862370247a98
b0n541 <j@nschaefer.net>
2023-12-01 23:01:04 +0100

    Add solution for day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,48 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+
+        return input
+            .map { line -> line.replace(Regex("[a-z]*"), "") }
+            .sumOf { numbers -> numbers.first().digitToIntOrNull()!! * 10 + numbers.last().digitToIntOrNull()!! }
     }
 
+    val numberAsStrings = mapOf(
+        "one" to '1',
+        "two" to '2',
+        "three" to '3',
+        "four" to '4',
+        "five" to '5',
+        "six" to '6',
+        "seven" to '7',
+        "eight" to '8',
+        "nine" to '9'
+    )
+
+    fun replaceWordWithDigit(line: String): String {
+        var result = line.toCharArray()
+        line.forEachIndexed { index, _ ->
+            numberAsStrings.keys.forEach { key ->
+                if (line.startsWith(key, index)) {
+                    result[index] = numberAsStrings[key]!!
+                }
+            }
+        }
+
+        return String(result)
+    }
+
+
     fun part2(input: List<String>): Int {
-        return input.size
+
+        return part1(input.map { line -> replaceWordWithDigit(line) })
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
+
+    val testInput2 = readInput("Day01_test2")
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
